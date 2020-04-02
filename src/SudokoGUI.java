554c5f3d49190a9c6c62256925ae57a76ecb3782
Esam Uddin <60678042+esam191@users.noreply.github.com>
2020-04-01 23:33:05 -0400

    using JComboBox to create the drop down menu

--- src/SudokoGUI.java
+++ src/SudokoGUI.java
@@ -30,6 +30,14 @@ public class SudokoGUI extends JFrame implements SwingConstants {
 		setPreferredSize(new Dimension(650,650));
 		setResizable(false);
 		
+		//Add menu item to let user choose a number from 1-9 
+		
+		int[] numberToChoose = {1 , 2, 3, 4, 5, 6, 7, 8, 9}'
+			JComboBox numbersList = new JComboBox (numbersToChoose);
+		JLabel nameofWindow = new JLabel();
+		
+		
+		
 		// Add the menu to support Undo operation
 		//Create the menu bar.
 		menuBar = new JMenuBar();
